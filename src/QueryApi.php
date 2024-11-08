8c698e8564a463fef3efb511a5c59dfb2ec18afb
samuelreichor <samuelreichor@gmail.com>
2024-11-08 12:11:13 +0100

    Change the api endpoints for more control

--- src/QueryApi.php
+++ src/QueryApi.php
@@ -43,9 +43,9 @@ class QueryApi extends Plugin
         Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_SITE_URL_RULES,
         function(RegisterUrlRulesEvent $event) {
             $event->rules = array_merge($event->rules, [
-              'GET /<version>/api/customQuery' => 'craft-query-api/default/get-custom-query-result',
-              'GET /<version>/api/allRoutes' => 'craft-query-api/default/get-all-routes',
-              'GET /<version>/api/allRoutes/<siteId>' => 'craft-query-api/default/get-all-routes',
+              'GET /<version>/api/queryApi/customQuery' => 'craft-query-api/default/get-custom-query-result',
+              'GET /<version>/api/queryApi/allRoutes' => 'craft-query-api/default/get-all-routes',
+              'GET /<version>/api/queryApi/allRoutes/<siteId>' => 'craft-query-api/default/get-all-routes',
           ]);
         }
     );
