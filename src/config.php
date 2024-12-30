2a4dd727b7efa33c192c724381d30d52a899e367
samuelreichor <samuelreichor@gmail.com>
2024-12-30 20:58:36 +0100

    Refactor example settings file

---
+++ src/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Craft Query API config.php
+ *
+ * This file exists only as a template for the Craft Query API settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'craft-query-api.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    '*' => [
+        // Defines the cache duration. Defaults to the cache duration defined in your general.php.
+        // 'cacheDuration' => 69420,
+        //
+        // Define field classes that should be excluded from the response.
+        // 'excludeFieldClasses' => ['nystudio107\seomatic\fields\SeoSettings']*/
+    ],
+];
