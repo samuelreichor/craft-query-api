4e59ec193848b5f09d8f65c0e3103080bc1269f9
samuelreichor <samuelreichor@gmail.com>
2024-12-30 20:58:36 +0100

    Refactor example settings file

--- src/config/craft-query-api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-return [
-    // Defines the cache duration. Defaults to the cache duration defined in your general.php.
-    'cacheDuration' => 69420,
-
-    // Define field classes that should be excluded from the response.
-    'excludeFieldClasses' => ['nystudio107\seomatic\fields\SeoSettings']
-];*/
