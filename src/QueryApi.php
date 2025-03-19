a649b90a4f8041f233bc8fcdaddc4c711a1b64cd
samuelreichor <samuelreichor@gmail.com>
2025-03-19 19:31:30 +0100

    Add cp sections

--- src/QueryApi.php
+++ src/QueryApi.php
@@ -22,6 +22,7 @@ use yii\log\FileTarget;
 class QueryApi extends Plugin
 {
     public string $schemaVersion = '1.0.0';
+    public bool $hasCpSection = true;
 
     public static function config(): array
     {
@@ -38,6 +39,11 @@ class QueryApi extends Plugin
 
         $this->initLogger();
         $this->attachEventHandlers();
+
+        if (Craft::$app->getRequest()->getIsCpRequest()) {
+            $this->_registerCpRoutes();
+        }
+
     }
 
     private function attachEventHandlers(): void
@@ -45,13 +51,30 @@ class QueryApi extends Plugin
         Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_SITE_URL_RULES,
         function(RegisterUrlRulesEvent $event) {
             $event->rules = array_merge($event->rules, [
-              'GET /<version>/api/queryApi/customQuery' => 'craft-query-api/default/get-custom-query-result',
-              'GET /<version>/api/queryApi/allRoutes' => 'craft-query-api/default/get-all-routes',
-              'GET /<version>/api/queryApi/allRoutes/<siteId>' => 'craft-query-api/default/get-all-routes',
+              'GET /<version>/api/queryApi/customQuery' => 'query-api/default/get-custom-query-result',
+              'GET /<version>/api/queryApi/allRoutes' => 'query-api/default/get-all-routes',
+              'GET /<version>/api/queryApi/allRoutes/<siteId>' => 'query-api/default/get-all-routes',
           ]);
-        }
-    );
+        });
     }
+
+    public function getCpNavItem(): ?array
+    {
+        $item = parent::getCpNavItem();
+        $item['subnav'] = [
+            'schemas' =>  [
+                'label' => 'Schemas',
+                'url' => 'query-api/schemas'
+            ],
+            'tokens' => [
+                'label' => 'Tokens',
+                'url' => 'query-api/tokens',
+            ]
+        ];
+        return $item;
+    }
+
+
 
     protected function createSettingsModel(): ?Model
     {
@@ -67,5 +90,28 @@ class QueryApi extends Plugin
         'logVars' => [],
     ]);
         Craft::getLogger()->dispatcher->targets[] = $logFileTarget;
+    }
+
+    private function _registerPermissions(): void
+    {
+
+    }
+
+    private function _registerCpRoutes(): void
+    {
+        Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function(RegisterUrlRulesEvent $event) {
+            $event->rules = array_merge($event->rules, [
+                'query-api' => ['template' => 'query-api/schemas/index.twig'],
+                'query-api/schemas/new' => ['template' => 'query-api/schemas/edit.twig'],
+                'query-api/schemas/edit/<schemaId:\d+>' => ['template' => 'query-api/schemas/edit.twig'],
+                'query-api/tokens/new' => ['template' => 'query-api/tokens/edit.twig'],
+                'query-api/tokens/edit/<tokenId:\d+>' => ['template' => 'query-api/tokens/edit.twig'],
+            ]);
+        });
+    }
+
+    private function _registerClearCaches(): void
+    {
+
     }
 }
